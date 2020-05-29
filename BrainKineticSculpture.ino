6ad791a48475e95ca7082ae5c4d55c85af98eb29
mtjjulian11 <61516486+mtjjulian11@users.noreply.github.com>
2020-05-29 10:13:06 +0200

    Starting to withdraw values of brain waves to analyse differentbehaviours, still not getting reads/ sec as the last sketch in brach Filter

--- BrainKineticSculpture.ino
+++ BrainKineticSculpture.ino
@@ -38,6 +38,17 @@ unsigned long LGdata_avg;
 unsigned long MGdata_avg;
 
 
+unsigned long Ddata_prom;
+unsigned long THdata_prom;
+unsigned long LAdata_prom;
+unsigned long HAdata_prom;
+unsigned long LBdata_prom;
+unsigned long HBdata_prom;
+unsigned long LGdata_prom;
+unsigned long MGdata_prom;
+
+
+
 
 
 void setup() {
@@ -148,250 +159,33 @@ void loop() {
       MGdata_avg +=  MGdata[i];
     }
            
-   
-    Serial.print(constrain((Ddata_avg / NUM_READINGS), 500, 999999));
+ Ddata_prom = Ddata_avg / NUM_READINGS;
+ THdata_prom = THdata_avg / NUM_READINGS;
+ LAdata_prom = LAdata_avg / NUM_READINGS;
+ HAdata_prom = HAdata_avg / NUM_READINGS;
+ LBdata_prom = LBdata_avg / NUM_READINGS;
+ HBdata_prom = HBdata_avg / NUM_READINGS;
+ LGdata_prom = LGdata_avg / NUM_READINGS;
+ MGdata_prom = MGdata_avg / NUM_READINGS;
+
+ 
+    Serial.print(constrain((Ddata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((THdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((THdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((LAdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((LAdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((HAdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((HAdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((LBdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((LBdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((HBdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((HBdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.print(constrain((LGdata_avg / NUM_READINGS), 500, 999999));
+    Serial.print(constrain((LGdata_prom), 500, 999999));
     Serial.print(" , ");
-    Serial.println(constrain((MGdata_avg / NUM_READINGS), 500, 999999));
+    Serial.println(constrain((MGdata_prom), 500, 999999));
     
     
   }
 
-
-
-
-
-  /*
-    Ddata_avg = 0;
-
-    //For de adquisición de datos
-    for (int i = 0; i < NUM_READINGS; i++) {
-    while (!brain.update()) {}
-    //Serial.println(brain.readErrors());
-    //Serial.println(brain.readCSV());
-    Ddata[i] = brain.readDelta();
-    Ddata_avg = Ddata_avg + Ddata[i];
-
-    Bdata[i] = brain.readBeta();
-    Bdata_avg = Bdata_avg + Bdata[i];
-
-    }
-
-    //For de impresión de datos y promedios
-    //Serial.print("------>>>>> Los valores leídos en loop son: ");
-    //for (int i = 0; i < NUM_READINGS; i++) {
-    //  Serial.print(Ddata[i]);
-    //  Serial.print(" ");
-    //}
-    Serial.print("El promedio es Delta es de ");
-    Serial.println(Ddata_avg / NUM_READINGS);
-
-    Serial.print("El prmedio de Beta es de ");
-    Serial.println(Bdata_avg / NUM_READINGS);
-  */
-
-  /*
-    // The .readCSV() function returns a string (well, char*) listing the most recent brain data, in the following format:
-    // "signal strength, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma"
-
-
-    if (brain.update()) {
-      for (int i = 0; i < NUM_READINGS; i++) {
-        Ddata[i] = brain.readDelta();
-      }
-    }
-
-
-
-
-
-
-
-
-    // ------------------------------- Intereaction
-
-    if (brain.readDelta() > SP_1) &&  (brain.readTheta() > SP_1) {
-
-
-
-    }
-    // --------- Constrain readings
-
-    ConD = constrain(brain.readDelta(), Min, Max);
-    ConTH = constrain(brain.readTheta(), Min, Max);
-    ConLA = constrain(brain.readLowAlpha(), Min, Max);
-    ConHA = constrain(brain.readHighAlpha(), Min, Max);
-    ConLB = constrain(brain.readLowBeta(), Min, Max);
-    ConHB = constrain(brain.readHighBeta(), Min, Max);
-    ConLG = constrain(brain.readLowGamma(), Min, Max);
-    ConMG = constrain(brain.readMidGamma(), Min, Max);
-
-
-
-    MapConD = map(ConD, Min, Max, 10.0, 1000.0);
-    MapConTH = map(ConTH, Min, Max, 10.0, 1000.0);
-    MapConLA = map(ConLA, Min, Max, 10.0, 1000.0);
-    MapConHA = map(ConHA, Min, Max, 10.0, 1000.0);
-    MapConLB = map(ConLB, Min, Max, 10.0, 1000.0);
-    MapConHB = map(ConHB, Min, Max, 10.0, 1000.0);
-    MapConLG = map(ConLG, Min, Max, 10.0, 1000.0);
-    MapConMG = map(ConMG, Min, Max, 10.0, 1000.0);
-
-    // ----------------------------   Mid Values For graph ------------
-
-
-
-
-
-    // ----------------------  Mid Vlues for Delta
-    SumDdata = OldDdata + MapConD;
-    GlobalDdata = SumDdata / Counter;
-    // ----------------------  Mid Vlues for Theta
-    SumTHdata = OldTHdata + MapConTH;
-    GlobalTHdata = SumTHdata / Counter;
-    // ----------------------  Mid Vlues for LowAlpha
-    SumLAdata = OldLAdata  + MapConLA ;
-    GlobalLAdata = SumLAdata / Counter;
-    // ----------------------  Mid Vlues for HighAlpha
-    SumHAdata = OldHAdata  + MapConHA ;
-    GlobalHAdata = SumHAdata / Counter;
-    // ----------------------  Mid Vlues for LowBeta
-    SumLBdata = OldLBdata  + MapConLB ;
-    GlobalLBdata = SumLBdata / Counter;
-    // ----------------------  Mid Vlues for HighBetaelta
-    SumHBdata = OldHBdata  + MapConHB ;
-    GlobalHBdata = SumHBdata / Counter;
-    // ----------------------  Mid Vlues for LowGamma
-    SumLGdata = OldLGdata  + MapConLG ;
-    GlobalLGdata = SumLGdata / Counter;
-    // ----------------------  Mid Vlues for Delta
-    SumMGdata = OldMGdata  + MapConMG ;
-    GlobalMGdata = SumMGdata / Counter;
-
-
-    // -----------------------  Print Values
-
-
-    Serial.print(GlobalDdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print(GlobalTHdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print( GlobalLAdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print(GlobalHAdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print(GlobalLBdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print(GlobalHBdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.print(GlobalLGdata);
-    Serial.print(",");
-    //delay(500);
-
-    Serial.println(GlobalMGdata);
-    //delay(500);
-
-
-    //    delay(1000);
-    //    Serial.print(GlobalDdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalTHdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalLAdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalHAdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalLBdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalHBdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.print(GlobalLGdata);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-    //    Serial.println(GlobalMGdata);
-    //    delay(1000);
-    //
-
-    //    Serial.print(Counter);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalDdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalTHdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalLAdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalHAdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalLBdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalHBdata);
-    //    Serial.print("  ,  ");
-    //    Serial.print(GlobalLGdata);
-    //    Serial.print("  ,  ");
-    //    Serial.println(GlobalMGdata);
-
-
-    // -----------------------   Storing The sum of the values
-
-    OldDdata = SumDdata;
-    OldTHdata = SumTHdata;
-    OldLAdata = SumLAdata;
-    OldHAdata = SumHAdata;
-    OldLBdata = SumLBdata;
-    OldHBdata = SumHBdata;
-    OldLGdata = SumLGdata;
-    OldMGdata = SumMGdata;
-
-    // -------------------------  Restart Avarege counter
-    //     This part of the code is intended to generate an avareg value of the brian
-    //     wave. It lets us guaranty accuracy if we go with the radar model were the values are
-    //     will update every 10 sec.
-
-
-    if (Counter >= Break) {
-      Counter = 0;
-      OldDdata = MapConD;
-      OldTHdata = MapConTH;
-      OldLAdata = MapConLA;
-      OldHAdata = MapConHA;
-      OldLBdata = MapConLB;
-      OldHBdata = MapConHB;
-      OldLGdata = MapConLG;
-      OldMGdata = MapConMG;
-    }
-    }
-    //
-    //    Serial.println(Counter);
-    //    Serial.print("  ,  ");
-    //    delay(1000);
-  */
 }
